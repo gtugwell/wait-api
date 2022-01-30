c795488b4c6c3d2ecfe7eeee8b6ed6f24a85dbb6
gtugwell <gtugwell8@gmail.com>
2022-01-30 03:53:42 -0600

    Changing port to dynamic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 30882
+const port = process.env.PORT || 3004
 
 app.use(bodyParser.json())
 app.use(function (req, res, next) {
