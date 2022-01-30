2b3c241a5fa45b3d2a57e41c12dee1cbf8216f9e
Shion-Ito <54923944+Shion-Ito@users.noreply.github.com>
2022-01-30 07:59:57 -0600

    Update index.js
    Updated to make post GET

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.listen(port, () => {
 
 const db = require('./queries')
 app.get('/destination/:dest', db.getDestination)
-app.post('/post', db.createPost)
+app.get('/post/:flight_num/:dest/:title/:image_url/:post_content/:tags', db.createPost)
 // app.get('/users/:id', db.getUserById)
 // app.post('/users', db.createUser)
 // app.put('/users/:id', db.updateUser)
