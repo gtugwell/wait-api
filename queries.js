16e1c3758ffeda5220dd394084112b9097ffa830
Shion-Ito <54923944+Shion-Ito@users.noreply.github.com>
2022-01-30 07:23:36 -0600

    Update queries.js

--- queries.js
+++ queries.js
@@ -18,20 +18,26 @@ const getDestination = (request, response) => {
     })
   }
 
-const createPost = (request, response) => {
-    const { flight_num, dest, title, image_url, post_content, tags } = request.body
+  const createPost = (request, response) => {
+    const flight_num = request.params.flight_num
+    const dest =  request.params.dest
+    const title = request.params.title
+    const image_url  = request.params.image_url
+    const post_content  = request.params.post_content
+    const tags = request.params.tags
 
     pool.query('INSERT INTO public.user_messages (flight_num, dest, title, image_url, post_content, tags) VALUES ($1, $2, $3, $4, $5, $6)', 
-    [flight_num, dest, title, image_url, post_content, tags], (error, results) => {
+    [flight_num, dest, title, image_url, post_content, tags], (error, result) => {
         if (error) {
         throw error
         }
-        response.status(201).send(`User added with ID: ${result.insertId}`)
+        response.status(201).send(`User added with ID: ${result}`)
     })
 }
 
 
+
 module.exports = {
-    getDestination,
-    createPost
+  getDestination,
+  createPost
   }
